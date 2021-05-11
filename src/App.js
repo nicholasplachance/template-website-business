f063a483727b7c0beb318f698a044dcb9bb3fe4b
Nicholas LaChance <nicholasplachance@gmail.com>
2021-05-11 12:28:43 -0500

    Create template repo and add responsive navigation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
 
+import BackDrop from './components/BackDrop/BackDrop';
+import NavBar from './components/NavBar/NavBar';
+import SideDrawer from './components/SideDrawer/SideDrawer';
+import { useState } from 'react';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [sideDrawerOpen, setSideDrawerOpen] = useState(false);
+
+	const drawerToggleClickHandler = () => {
+		setSideDrawerOpen((prevState) => {
+			return { sideDrawerOpen: !prevState.setSideDrawerOpen };
+		});
+	};
+
+	const backDropClickHandler = () => {
+		setSideDrawerOpen(false);
+	};
+
+	let sideDrawer;
+	let backdrop;
+
+	if (sideDrawerOpen) {
+		sideDrawer = <SideDrawer />;
+		backdrop = <BackDrop click={backDropClickHandler} />;
+	}
+
+	return (
+		<div className='App'>
+			<NavBar drawerToggleClickHandler={drawerToggleClickHandler} />
+			<SideDrawer show={sideDrawerOpen} />
+			{backdrop}
+			<main style={{ marginTop: '69px' }}></main>
+			<p>
+				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, magni natus enim sit recusandae non
+				maiores eveniet voluptatum, harum dignissimos rem repellendus at temporibus sed dolorem omnis soluta
+				aliquid eos?
+			</p>
+		</div>
+	);
 }
 
 export default App;
