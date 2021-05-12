2e728ca8f58a42aa0b195e86e8c920c76ee5da24
Nicholas LaChance <nicholasplachance@gmail.com>
2021-05-12 14:11:20 -0500

    Add section components and contianer component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 
 import BackDrop from './components/BackDrop/BackDrop';
+import Container from './components/Container/Container';
 import HeroBanner from './components/HeroBanner/HeroBanner';
 import NavBar from './components/NavBar/NavBar';
+import Section from './components/Sections/Section';
 import SideDrawer from './components/SideDrawer/SideDrawer';
 import { useState } from 'react';
 
@@ -33,6 +35,12 @@ function App() {
 			<SideDrawer show={sideDrawerOpen} />
 			{backdrop}
 			<HeroBanner />
+			<main style={{ marginTop: '280px' }}></main>
+			<Container>
+				<Section />
+				<Section />
+				<Section />
+			</Container>
 			<p>
 				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, magni natus enim sit recusandae non
 				maiores eveniet voluptatum, harum dignissimos rem repellendus at temporibus sed dolorem omnis soluta
